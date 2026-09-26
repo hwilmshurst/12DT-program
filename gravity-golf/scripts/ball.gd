856b7c01d1061a3d70363168c39740ed8a6fdb1b
hwilmshurst <hwilmshurst@students.mags.school.nz>
2026-09-27 00:14:44 +1200

    Gravity power control

--- gravity-golf/scripts/ball.gd
+++ gravity-golf/scripts/ball.gd
@@ -1,5 +1,6 @@
 extends RigidBody2D
 
+
 # dictionary that stores the different directions the ball can travel
 var directions = [
 	Vector2.DOWN,
@@ -11,15 +12,19 @@ var directions = [
 
 # variable that stores the starting gravity strength
 @export var gravity_strength: float = 200.0
+# Stores the gravity multiplier
+@export var gravity_multiplier: float = 1.0
+
+
 
 #variable that stores the starting gravity direction
 var gravity_direction = Vector2.DOWN
 var current_direction = 0
+var gravity_change_speed: float = 1
 
 
 func _physics_process(delta):
-	#function that applies gravity to the ball
-	apply_central_force(gravity_direction * gravity_strength)
+	apply_central_force(gravity_direction * gravity_strength * gravity_multiplier)
 
 
 func _process(delta):
@@ -30,3 +35,12 @@ func _process(delta):
 	if Input.is_action_just_pressed("ui_accept"):
 		current_direction = (current_direction + 1) % 4
 		gravity_direction = directions[current_direction]
+			# Increase gravity with Up Arrow
+	# Gradually increase gravity while Up Arrow is held
+	if Input.is_key_pressed(KEY_UP):
+		gravity_multiplier += gravity_change_speed * delta
+	# Gradually decrease gravity while Down Arrow is held
+	if Input.is_key_pressed(KEY_DOWN):
+		gravity_multiplier -= gravity_change_speed * delta
+	# Keep multiplier between 1.00x and 3.00x
+	gravity_multiplier = clamp(gravity_multiplier, 0.25, 3.0)
